096a1d24962ba99e1343749fba0c534d2627a6d1
Deekshya Karki <99348458+dishy2001@users.noreply.github.com>
2023-01-27 17:32:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
-
+	let s = str.toLowerCase();
+	let n = s.length()-1;
+	for(let i = 0; i < s.length(); i++){
+		if(s[i] !== s[n]){
+			return false;
+		}
+		n--;
+	}
+	return true;
 }
 module.exports = palindrome
